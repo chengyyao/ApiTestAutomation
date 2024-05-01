f65f9f5485a3a136f6178c02b26016a40734f4a9
yaocy <yaocy@glodon.com>
2024-05-01 13:47:50 +0800

    增加参数化时随机生成名称

--- src/main/java/com/ycy/api/test/core/Strategy.java
+++ src/main/java/com/ycy/api/test/core/Strategy.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * @author yaocy
@@ -20,16 +21,19 @@ public interface Strategy<T> {
 
     String authorizationFilePath = "Authorization.conf";
 
+    static String randomStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     String META_DATA_FILE = "data";
     String META_PARAMS_FILE = "params";
+
     ArrayList<JsonBean> process(T t);
 
 
     /**
-     * @param file 则传入 /BusinessManagement/data/ExternalServiceAgreement_c_1.yaml
+     * @param file
      * @param jsonBean
      */
-    default void combParams(File file, JsonBean jsonBean){
+    default void combParams(File file, JsonBean jsonBean) {
         // 根据传入的文件路径，解析文件内容为字符串，然后取authorization 的值 set到jsonBean中
         File authorizationFile = new File(getResourcePath() + authorizationFilePath);
         //文件转成字符串
@@ -44,22 +48,42 @@ public interface Strategy<T> {
         for (Map.Entry<String, Object> stringObjectEntry : jsonObject.entrySet()) {
             jsonBean.setUrl(replaceParams(stringObjectEntry, jsonBean.getUrl()));
             jsonBean.setBody(replaceParams(stringObjectEntry, jsonBean.getBody()));
-            jsonBean.setPostUrl(replaceParams(stringObjectEntry,jsonBean.getPostUrl()));
+            jsonBean.setPostUrl(replaceParams(stringObjectEntry, jsonBean.getPostUrl()));
 
         }
 
 
     }
 
-    default String replaceParams(Map.Entry<String, Object> stringObjectEntry, String oldStr){
-        return oldStr.replace("{" + stringObjectEntry.getKey() + "}", stringObjectEntry.getValue().toString());
+    default String replaceParams(Map.Entry<String, Object> stringObjectEntry, String oldStr) {
+        return oldStr.replace("{" + stringObjectEntry.getKey() + "}", stringObjectEntry.getValue().toString() + getRandomStr());
     }
 
-    default String getResourcePath(){
+    default String getResourcePath() {
         URL resourcePathUrl = getClass().getClassLoader().getResource("");
         if (Objects.isNull(resourcePathUrl)) throw new CustomizeException();
         return resourcePathUrl.getPath();
 
+    }
+
+    default String getRandomStr() {
+        // 创建一个包含大写字母和数字的字符数组
+        char[] chars = randomStr.toCharArray();
+
+        // 创建一个StringBuilder来构建最终的随机字符串
+        StringBuilder sb = new StringBuilder();
+
+        // 创建Random对象用于生成随机数
+        Random random = new Random();
+
+        // 生成5个随机字符
+        for (int i = 0; i < 5; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+
+        // 将StringBuilder转换为字符串
+        return sb.toString();
     }
 
 }
